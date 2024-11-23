1a73f9652f54100aba3f3d8c28adb6bf1b6dc4c4
Sumit Kumar Panika <sumitkp153@gmail.com>
2024-11-23 22:30:42 +0530

    Update home.js

--- Scripts/home.js
+++ Scripts/home.js
@@ -115,7 +115,7 @@ function checkSignInStatus() {
     } else {
         signInButton.textContent = 'Sign In';
         signInButton.onclick = () => {
-            window.location.href = '/Pages/sign_in.html'; // Redirect to sign-in page
+            window.location.href = '/Book_Vault/Pages/sign_in.html'; // Redirect to sign-in page
         };
 
         signOutButton.style.display = 'none';
@@ -125,7 +125,7 @@ function checkSignInStatus() {
 // Function to handle the sign-out action
 function signOut() {
     localStorage.removeItem('user'); // Remove user data from localStorage
-    window.location.href = "/index.html"; // Redirect to home page
+    window.location.href = "/Book_Vault/index.html"; // Redirect to home page
 }
 
 // Nav bar toggle for mobile menu
