1154055887ae6fd7be30dbd706c3ef868ff02536
11-sg <sg4242674@gmail.com>
2026-07-10 21:21:02 +0530

    api-key

--- chatbot/chat.js
+++ chatbot/chat.js
@@ -12,7 +12,8 @@ let isResponseGenerating = false;
 const API_KEY = "AQ.Ab8RN6KHTwkIYlKwMuw10IFgZ9iFeDA6x890aQUfiE5-OgookQ"; // Your API key here
 // AQ.Ab8RN6KHTwkIYlKwMuw10IFgZ9iFeDA6x890aQUfiE5-OgookQ
 
-const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-3-flash-preview:generateContent?key=${API_KEY}`;
+// const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key=${API_KEY}`;
+const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
 
 const inputInitHeight = chatInput.scrollHeight;
 
